a37bb5eba51187704817c1474bfc60beacb31885
antonioNascimento <antonioo_junior@hotmail.com>
2015-08-03 16:49:56 -0300

    Commit Inicial

---
+++ desktop/src/br/grupointegrado/spaceinvaders/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.grupointegrado.spaceinvaders.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.grupointegrado.spaceinvaders.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
